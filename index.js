8bef2b65f83a52d2a8b42b248dee56af604883be
Aaron W. West <aphexlog@gmail.com>
2021-12-23 20:36:34 -0600

    messing around  w some routes

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ const PORT = 8000
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const res = require('express/lib/response');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json('Welcome!!')
+    res.status(401)
+    res.json({
+        message: 'test',
+        blah: 'fucku'
+    })
 });
 
 app.get('/test', (req, res) => {
     res.json('Test')
+})
+
+app.post('/fu', (req, res) => {
+    var generator = require('generate-password');
+
+    var password = generator.generate({
+        length: 10,
+        numbers: true
+    });
+
+    res.json({
+        password: password
+    })
+})
+app.get('/fu', (req, res) => {
+    res.json({
+        message: 'success'
+    })
 })
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
